1ae58e6d6d29863878d2b2f16469ab7560c2a5c3
Jeff Sulfranc <sulfranc@gmail.com>
2021-12-14 21:59:56 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+console.log(document.domain)
+console.log(document.URL)
+console.log(document.title)
+//document.title 
+console.log(document.doctype)
+console.log(document.head)
+console.log(document.newHeader)
+document.getElementById('main')
+const newHeader = document.getElementById('victory')
+// let h1#victory = document.createElement (aChild)
+const getElementById = ele 
